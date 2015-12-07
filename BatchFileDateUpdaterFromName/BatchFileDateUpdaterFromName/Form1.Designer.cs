4cd6988d5ccabc8d9fa0151d4823edd07fe32e76
oguzhankahyaoglu <oguzhan.kahyaoglu@yahoo.com>
2015-12-07 15:23:22 +0200

    first commit (maybe the last)

---
+++ BatchFileDateUpdaterFromName/BatchFileDateUpdaterFromName/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace BatchFileDateUpdaterFromName
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnUpdateFiles = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.btnFindFiles = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.FullRowSelect = true;
+            this.listView1.Location = new System.Drawing.Point(13, 35);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(877, 412);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Dosya Adı";
+            this.columnHeader1.Width = 200;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Orj. Olş. Tarihi";
+            this.columnHeader2.Width = 150;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Orj. Dğş. Trh.";
+            this.columnHeader3.Width = 150;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Yeni Tarih";
+            this.columnHeader4.Width = 200;
+            // 
+            // btnUpdateFiles
+            // 
+            this.btnUpdateFiles.Location = new System.Drawing.Point(765, 454);
+            this.btnUpdateFiles.Name = "btnUpdateFiles";
+            this.btnUpdateFiles.Size = new System.Drawing.Size(124, 23);
+            this.btnUpdateFiles.TabIndex = 1;
+            this.btnUpdateFiles.Text = "Update Date Times";
+            this.btnUpdateFiles.UseVisualStyleBackColor = true;
+            this.btnUpdateFiles.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(13, 10);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(105, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Include Subdirs?";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // btnFindFiles
+            // 
+            this.btnFindFiles.Location = new System.Drawing.Point(124, 6);
+            this.btnFindFiles.Name = "btnFindFiles";
+            this.btnFindFiles.Size = new System.Drawing.Size(75, 23);
+            this.btnFindFiles.TabIndex = 3;
+            this.btnFindFiles.Text = "Find files";
+            this.btnFindFiles.UseVisualStyleBackColor = true;
+            this.btnFindFiles.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(902, 490);
+            this.Controls.Add(this.btnFindFiles);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.btnUpdateFiles);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button btnUpdateFiles;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button btnFindFiles;
+
+    }
+}
+
---
+++ BatchFileDateUpdaterFromName/BatchFileDateUpdaterFromName/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BatchFileDateUpdaterFromName
+{
+    public partial class Form1 : Form
+    {
+        public static String ParseFormat = ConfigurationManager.AppSettings["DateTimeParseFormat"];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private String Parse(String s, out DateTime? parsedDate)
+        {
+            parsedDate = null;
+            try
+            {
+                var name = s.Substring(0, s.LastIndexOf('.'));
+                parsedDate = DateTime.ParseExact(name, ParseFormat, CultureInfo.InvariantCulture);
+                return parsedDate.ToString();
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var path = folderBrowserDialog1.SelectedPath;
+            var directory = new DirectoryInfo(path);
+            var files = directory.GetFiles("*", checkBox1.Checked ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly);
+            var count = 0;
+            foreach (var file in files)
+            {
+                DateTime? parsedDate;
+                Parse(file.Name, out parsedDate);
+                if (parsedDate == null) 
+                    continue;
+                file.LastWriteTime = parsedDate.Value;
+                file.CreationTime = parsedDate.Value;
+                count ++;
+            }
+            MessageBox.Show("Updated " + count + " files!");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            var path = folderBrowserDialog1.SelectedPath;
+            var directory = new DirectoryInfo(path);
+            var files = directory.GetFiles("*", checkBox1.Checked ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly);
+            listView1.Items.Clear();
+            foreach (var file in files)
+            {
+                var createDate = file.CreationTime.ToString();
+                var modifyDate = file.LastWriteTime.ToString();
+                DateTime? parsedDate;
+                var dateStr = Parse(file.Name, out parsedDate);
+                var item = new ListViewItem(new[] { file.Name, createDate, modifyDate, dateStr ?? "" });
+                listView1.Items.Add(item);
+            }
+        }
+    }
+}
